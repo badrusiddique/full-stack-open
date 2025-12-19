cc1c1bf14f480cab63ad37aa96975dcb24f0b4a1
Badru <badru@buckytechnologies.com>
2025-12-20 00:10:31 +0530

    including object based render

--- 1-full-stack/1-vite-intro/src/App.jsx
+++ 1-full-stack/1-vite-intro/src/App.jsx
@@ -3,11 +3,17 @@ import React, { useState, useEffect } from 'react';
 const Hello = ({ name = "Stranger", age }) => {
   const anAdult = parseInt(age) >= 18 ? "an ADULT" : "a MINOR";
 
-  return <p>Hello, {name}!, you are {anAdult}</p>;
+  return <li>Hello, {name}!, you are {anAdult}</li>;
 }
 
 
 const App = () => {
+  const friends = [
+    { name: 'George', age: 41 },
+    { name: 'Daisy', age: 35 },
+    { name: 'Matt', age: 13 },
+    { },
+  ];
   const [time, setTime] = useState(new Date());
 
   useEffect(() => {
@@ -22,13 +28,16 @@ const App = () => {
 
 
   return (
-    <div>
+    <>
       <h3>Welcome to Vite + React! (at {time.toLocaleTimeString('en-US', { hour12: true })})</h3>
-      <Hello name='George' age="41" />
-      <Hello name='Daisy' age={35} />
-      <Hello name='Matt' age="13" />
-      <Hello />
-    </div>
+      <ul>
+        {
+          friends.map(({ name, age }) => (
+            <Hello key={name} name={name} age={age} />
+          ))
+        }
+      </ul>
+    </>
   );
 }
 
