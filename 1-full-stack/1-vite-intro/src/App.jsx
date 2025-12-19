b37d37f9bbd3dc99ffef1a52ef46bce5559d0432
Badru <badru@buckytechnologies.com>
2025-12-19 23:54:18 +0530

    including timer using set interval for multi component

--- 1-full-stack/1-vite-intro/src/App.jsx
+++ 1-full-stack/1-vite-intro/src/App.jsx
@@ -1,8 +1,33 @@
+import React, { useState, useEffect } from 'react';
 
-function App() {
+const Hello = ({ name = "Stranger", age }) => {
+  const anAdult = parseInt(age) >= 18 ? "an ADULT" : "a MINOR";
+
+  return <p>Hello, {name}!, you are {anAdult}</p>;
+}
+
+
+const App = () => {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      // update time every second
+      setTime(new Date());
+    }, 1000);
+
+    // unmount cleanup
+    return () => clearInterval(timer);
+  }, []);
+
+
   return (
     <div>
-      <h1>Welcome to Vite + React!</h1>
+      <h3>Welcome to Vite + React! (at {time.toLocaleTimeString('en-US', { hour12: true })})</h3>
+      <Hello name='George' age="41" />
+      <Hello name='Daisy' age={35} />
+      <Hello name='Matt' age="13" />
+      <Hello />
     </div>
   );
 }
